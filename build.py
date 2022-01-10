13e4b676e98e2edd385b51253cdc6ca1735574fb
harryyo <56394038+harryyo@users.noreply.github.com>
2022-01-10 18:06:20 +0000

    Improved PEP8 styles

--- build.py
+++ build.py
@@ -2,16 +2,17 @@ import urllib.request
 import zipfile
 import shutil
 import os
+
 print("Downloading static file zip.")
-url = 'https://github.com/alphagov/govuk-frontend/releases/download/v4.0.0/release-v4.0.0.zip'
-urllib.request.urlretrieve(url, './govuk_frontend.zip')
+url = "https://github.com/alphagov/govuk-frontend/releases/download/v4.0.0/release-v4.0.0.zip"
+urllib.request.urlretrieve(url, "./govuk_frontend.zip")
 print("Deleting old app/static")
 try:
     shutil.rmtree("./app/static")
 except FileNotFoundError:
     print("No old app/static to remove.")
 print("Unzipping file to app/static...")
-with zipfile.ZipFile("./govuk_frontend.zip", 'r') as zip_ref:
+with zipfile.ZipFile("./govuk_frontend.zip", "r") as zip_ref:
     zip_ref.extractall("./app/static")
 print("Moving files from app/static/assets to app/static")
 for file_to_move in os.listdir("./app/static/assets"):
