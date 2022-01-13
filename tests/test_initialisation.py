d9c8bda9cf9d1412b9226f29b931f824f423db3f
jharrington <jack.harrington@evoco.co.uk>
2022-01-13 17:47:36 +0000

    Renamed tests.

--- tests/test_initialisation.py
+++ tests/test_initialisation.py
@@ -1,3 +1,8 @@
+"""
+Some very basic tests. Tests if the flask client spins
+up and serves the correct pages.
+This is the most basic set of tests.
+"""
 # flake8: noqa: F811
 from tests.testconf import flask_test_client  # noqa
 
@@ -8,7 +13,7 @@ def test_flask_initiates(flask_test_client):
     WHEN the '/' page (index) is requested (GET)
     THEN check that the get response is successful.
     If this test succeedes then our flask application
-    is ATLEAST up and running.
+    is ATLEAST up and running without errors.
     """
     response = flask_test_client.get("/", follow_redirects=True)
     assert response.status_code == 200
@@ -18,7 +23,7 @@ def test_helloworld_homepage(flask_test_client):
     """
     GIVEN Our Flask Hello World Application
     WHEN the '/' page (index) is requested (GET)
-    THEN check that the homepage contains the phrase
+    THEN check that the homepage (/) contains the phrase
     "Hello World".
     """
     response = flask_test_client.get("/", follow_redirects=True)
