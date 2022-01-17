eb16ac41ed96a845db496bc3cd33ed1b971ab7ff
harryyo <56394038+harryyo@users.noreply.github.com>
2022-01-17 17:38:09 +0000

    Docstrings added and excess comments removed

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,14 +1,18 @@
 import pytest
 from app.create_app import create_app
-from chromedriver_py import binary_path  # this will get you the path variable
+from chromedriver_py import binary_path
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 
 
-# Fixture for the Flask app
+# Fixture of app instance
 @pytest.fixture(scope="session")
 def app():
+    """Returns an instance of the Flask app as a fixture for testing
+    which is available for the testing session and accessed with the
+    @pytest.mark.uses_fixture('live_server')"""
+
     app = create_app()
     return app
 
@@ -16,14 +20,16 @@ def app():
 # Fixture for Chrome
 @pytest.fixture(scope="class")
 def selenium_chrome_driver(request):
-    # print(binary_path)
+    """Returns a Selenium Chrome driver as a fixture for testing,
+    using an installed Chromedriver from the .venv chromedriver_py package
+    install location. Accessible with the
+    @pytest.mark.uses_fixture('selenium_chrome_driver')"""
+
     # TODO: Consider using hitchchrome 85.0 to install compatible versions of
     #  chromedriver, chrome and selenium simultaneously
     service_object = Service(binary_path)
     chrome_options = Options()
-    chrome_options.add_argument(
-        "--headless"
-    )  # chrome_options.headless = True # also works
+    chrome_options.add_argument("--headless")
     # TODO: set chrome_options.binary_location = ...
     #  (if setting to run in container or on GitHub)
     chrome_driver = webdriver.Chrome(
