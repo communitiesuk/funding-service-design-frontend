d63276669eea30b0bd2a0a5b1bb91e4a552e9f7e
harryyo <56394038+harryyo@users.noreply.github.com>
2022-01-17 11:24:50 +0000

    Default session cookie name added

--- app/config.py
+++ app/config.py
@@ -12,6 +12,6 @@ class Config:
     """Base config."""
 
     SECRET_KEY = environ.get("SECRET_KEY") or "dev"
-    SESSION_COOKIE_NAME = environ.get("SESSION_COOKIE_NAME")
+    SESSION_COOKIE_NAME = environ.get("SESSION_COOKIE_NAME") or "session_cookie"  # noqa
     STATIC_FOLDER = "static"
     TEMPLATES_FOLDER = "templates"
