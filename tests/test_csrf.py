0296001af055c5111dd410f1c4e974b66c3fc000
Ram <rmn24in@gmail.com>
2022-01-18 12:39:15 +0000

    Added OWASP ZAP scan to .github yaml

--- tests/test_csrf.py
+++
@@ -1,7 +0,0 @@
-def test_csrf_files(flask_test_client):
-    """
-    The test should return unauthorised message
-     if the request is not received from https server
-    """
-    response = flask_test_client.post("/", follow_redirects=True)
-    assert response.status_code == 401
