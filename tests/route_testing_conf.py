856b185fb76ce0845fc282643a146c3499b2e1e1
Steve Walton <stevewalton@stevewaltonsmbp.lan>
2022-01-21 15:15:06 +0000

    Fixing tests

--- tests/route_testing_conf.py
+++ tests/route_testing_conf.py
@@ -4,4 +4,4 @@ Our single source of truth for which
 routes need to be tested and their expected
 content.
 """
-routes_and_test_content = {"/": b"Hello World"}
+routes_and_test_content = {"/": b"Hello Gemma"}
