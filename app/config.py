09986c41e75173cd4e2c5a0ab1d916dc93c47bc9
harryyo <56394038+harryyo@users.noreply.github.com>
2022-01-28 14:30:59 +0000

    FORMS_SERVICE... env variables renamed and reduced to PUBLIC_HOST, PREVIEW_HOST and JSONS_PATH to simplify config following update of tests to automatically check all add forms

--- app/config.py
+++ app/config.py
@@ -8,19 +8,17 @@ SECRET_KEY = environ.get("SECRET_KEY") or "dev"
 SESSION_COOKIE_NAME = environ.get("SESSION_COOKIE_NAME") or "session_cookie"
 STATIC_FOLDER = "static"
 TEMPLATES_FOLDER = "templates"
+LOCAL_SERVICE_NAME = "local_flask"
 
 """
 Forms Service Config
 """
 FORMS_SERVICE_NAME = environ.get("FORMS_SERVICE_NAME") or "xgov_forms_service"
-FORMS_SERVICE_HOST = (
+FORMS_SERVICE_PUBLIC_HOST = (
     environ.get("FORMS_SERVICE_HOST") or "http://localhost:3009"
 )
-FORMS_SERVICE_TEST_PUBLIC_FORM = (
-    environ.get("FORMS_SERVICE_TEST_PUBLIC_FORM") or 0
-)
-FORMS_SERVICE_TEST_FORM = (
-    environ.get("FORMS_SERVICE_TEST_FORM") or "simple-three-step-form"
+FORMS_SERVICE_PREVIEW_HOST = (
+    environ.get("FORMS_SERVICE_HOST") or "http://localhost:3009"
 )
 FORMS_SERVICE_JSONS_PATH = (
     environ.get("FORMS_SERVICE_JSONS_PATH") or "form_jsons"
