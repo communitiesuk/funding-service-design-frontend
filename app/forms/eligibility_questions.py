07a17f6e9ccc7fd9a4941372325fa13ed4535efd
jharrington <jack.harrington@evoco.co.uk>
2022-02-10 13:39:58 +0000

    set up

---
+++ app/forms/eligibility_questions.py
@@ -0,0 +1,13 @@
+from flask_wtf import FlaskForm
+
+
+def minimium_funding_amount_page(minimium_money_amount):
+    class MinimiumForm(FlaskForm):
+
+        pass
+
+    def minimium_cost_page():
+
+        pass
+
+    return minimium_cost_page
