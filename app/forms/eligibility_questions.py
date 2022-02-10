033facd40d53729404549e81b7256da915dc099f
jharrington <jack.harrington@evoco.co.uk>
2022-02-10 13:49:00 +0000

    less abstract implementation

--- app/forms/eligibility_questions.py
+++ app/forms/eligibility_questions.py
@@ -1,4 +1,3 @@
-from app.data_models.eligibility_criteria import eligibility_criterion
 from flask import redirect
 from flask import render_template
 from flask_wtf import FlaskForm
@@ -7,43 +6,19 @@ from wtforms.validators import DataRequired
 from wtforms.validators import NumberRange
 
 
-def form_page_from_criterion(criterion: eligibility_criterion):
+def form_page_from_criterion(min_amount: int):
     class criterionForm(FlaskForm):
 
         criterion_field = Field(
-            label=criterion.input_name,
-            validators=[DataRequired(), criterion.eligibility_validator],
+            label="project_money_amount",
+            validators=[DataRequired(), NumberRange(min_amount)],
         )
 
     def criterion_page():
 
         form = criterionForm()
-        input_arg_dict = {
-            "label": {"text": criterion.eligibility_question},
-            "id": form.criterion_field.id,
-            "name": form.criterion_field.id,
-        } | criterion.eligibility_component_options
         if form.validate_on_submit():
             return redirect("/")
-        return render_template(
-            "funding_amount.html", input_dict=input_arg_dict
-        )
+        return render_template("min_funding_amount.html", form=form)
 
     return criterion_page()
-
-
-def must_be_atleast_criterion(min_amount):
-    return eligibility_criterion(
-        input_name="amount_needed",
-        eligibility_component="govukInput",
-        eligibility_question="How much money do you need?",
-        eligibility_validator=NumberRange(min_amount),
-        failed_eligibility_reason=(
-            f"You must be applying for atleast £{min_amount}"
-        ),
-        eligibility_component_options={
-            "label": {"text": "Enter your answer using numbers only"},
-            "prefix": {"text": "£"},
-            "classes": "govuk-input--width-20",
-        },
-    )
