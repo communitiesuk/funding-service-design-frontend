0be6cc57fb393fbe01eb5c163f1220a2c2d48f61
jharrington <jack.harrington@evoco.co.uk>
2022-02-16 16:39:34 +0000

    Service url is env var

--- app/default/routes.py
+++ app/default/routes.py
@@ -2,6 +2,7 @@ from app.forms.eligibility_questions import minimium_money_question_page
 from flask import Blueprint
 from flask import render_template
 from flask import url_for
+import flask.config
 
 default_bp = Blueprint("routes", __name__, template_folder="templates")
 
@@ -12,14 +13,11 @@ def index():
         "index.html", service_url=url_for("routes.max_funding_criterion")
     )
 
-
 @default_bp.route("/funding_amount_eligibility", methods=["GET", "POST"])
 def max_funding_criterion():
     return minimium_money_question_page(
         1000,
-        "https://funding-service-design-form-runner."
-        "london.cloudapps.digital/funding-application"
-        "/about-you",
+         flask.config["FORMS_SERVICE_PUBLIC_HOST"]
     )
 
 @default_bp.route("/not-eligible")
