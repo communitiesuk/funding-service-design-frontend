4a61497405f44dad6f4ed4c40a877267fbb67402
J <65361824+SentientHousePlant@users.noreply.github.com>
2022-03-24 15:36:35 +0000

    skjdfkgbsdfkjh

--- app/default/routes.py
+++ app/default/routes.py
@@ -1,8 +1,8 @@
+from app.config import FORMS_SERVICE_PUBLIC_HOST
 from app.forms.eligibility_questions import minimium_money_question_page
 from flask import Blueprint
 from flask import render_template
 from flask import url_for
-from app.config import FORMS_SERVICE_PUBLIC_HOST
 
 default_bp = Blueprint("routes", __name__, template_folder="templates")
 
@@ -13,18 +13,16 @@ def index():
         "index.html", service_url=url_for("routes.max_funding_criterion")
     )
 
+
 @default_bp.route("/funding_amount_eligibility", methods=["GET", "POST"])
 def max_funding_criterion():
-    return minimium_money_question_page(
-        1000,
-         FORMS_SERVICE_PUBLIC_HOST
-    )
+    return minimium_money_question_page(1000, FORMS_SERVICE_PUBLIC_HOST)
+
 
 @default_bp.route("/not-eligible")
 def not_eligible():
-    return render_template(
-    "not_eligible.html"
-    )
+    return render_template("not_eligible.html")
+
 
 @default_bp.errorhandler(404)
 def not_found(error):
