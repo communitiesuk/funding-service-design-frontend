3ba22209c6dcf6bdc710c4ca8f5dc09a023b8c3a
Gideon Goldberg <1764158+gidsg@users.noreply.github.com>
2022-05-23 17:46:47 +0100

    update table

--- app/models/application_summary.py
+++ app/models/application_summary.py
@@ -1,6 +1,7 @@
 import inspect
 from dataclasses import dataclass
 from datetime import datetime
+from typing import Optional
 
 
 @dataclass
@@ -10,10 +11,12 @@ class ApplicationSummary:
     round_id: str
     fund_id: str
     started_at: datetime
-
+    project_name: str
+    last_edited: Optional[datetime] = None
 
     def __post_init__(self):
         self.started_at = datetime.fromisoformat(self.started_at)
+        self.last_edited = datetime.fromisoformat(self.last_edited) if self.last_edited else None
 
     @classmethod
     def from_dict(cls, env):
