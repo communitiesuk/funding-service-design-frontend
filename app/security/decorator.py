3708191da98b91257570d368039e9c51009fb1e8
Gideon Goldberg <1764158+gidsg@users.noreply.github.com>
2022-05-31 14:00:58 +0100

    handle errors inside check method

--- app/security/decorator.py
+++ app/security/decorator.py
@@ -2,6 +2,7 @@ from functools import wraps
 
 from app import config
 from app.security.utils import validate_token
+from flask import abort
 from flask import redirect
 from flask import request
 from jwt import ExpiredSignatureError
@@ -9,18 +10,23 @@ from jwt import ExpiredSignatureError
 
 def _failed_redirect():
     return redirect(
-        f"{config.AUTHENTICATOR_HOST}/magic-links/"
-        "new?error=Link+expired+or+invalid"
+        abort(
+            redirect(
+                f"{config.AUTHENTICATOR_HOST}/magic-links/"
+                "new?error=Link+expired+or+invalid"
+            )
+        )
     )
 
 
 def _check_access_token():
-
     login_cookie = request.cookies.get(config.FSD_USER_TOKEN_COOKIE_NAME)
     if not login_cookie:
+        _failed_redirect()
+    try:
+        return validate_token(login_cookie)
+    except ExpiredSignatureError:
         return _failed_redirect()
-
-    return validate_token(login_cookie)
 
 
 def login_required(f):
@@ -29,10 +35,7 @@ def login_required(f):
 
     @wraps(f)
     def decorated(*args, **kwargs):
-        try:
-            token_payload = _check_access_token()
-        except ExpiredSignatureError:
-            return _failed_redirect()
+        token_payload = _check_access_token()
         kwargs["account_id"] = token_payload.get("accountId")
         return f(*args, **kwargs)
 
