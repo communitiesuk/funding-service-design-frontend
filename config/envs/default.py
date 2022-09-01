561761524f47870be8d336a0c1e822f393d9ebc7
harryyo <56394038+harryyo@users.noreply.github.com>
2022-09-01 10:02:17 +0100

    Pass RSA secret env variables through manifest

--- config/envs/default.py
+++ config/envs/default.py
@@ -1,4 +1,5 @@
 """Flask configuration."""
+import base64
 from os import environ
 from os import getenv
 from pathlib import Path
@@ -24,7 +25,7 @@ class DefaultConfig:
     ENTER_APPLICATION_URL = AUTHENTICATOR_HOST + "/service/magic-links/new"
     SESSION_COOKIE_DOMAIN = environ.get("SESSION_COOKIE_DOMAIN")
     RSA256_PUBLIC_KEY_BASE64 = environ.get("RSA256_PUBLIC_KEY_BASE64")
-    RSA256_PUBLIC_KEY = environ.get("RSA256_PUBLIC_KEY")
+    RSA256_PUBLIC_KEY = base64.b64decode(RSA256_PUBLIC_KEY_BASE64).decode()
 
     # APIs Config
     TEST_APPLICATION_STORE_API_HOST = "http://application_store"
--- config/envs/production.py
+++ config/envs/production.py
@@ -1,14 +1,8 @@
 """Flask Production Environment Configuration."""
-import base64
-from os import environ
-
 from config.envs.default import DefaultConfig
 from fsd_utils import configclass
 
 
 @configclass
 class ProductionConfig(DefaultConfig):
-
-    RSA256_PUBLIC_KEY = base64.b64decode(
-        environ.get("RSA256_PUBLIC_KEY_BASE64")
-    ).decode()
+    pass
