e61b2c198f8fc203b8ace21c86de70ee06ff1ff8
Sarah Sloan <sarah.sloan@levellingup.gov.uk>
2023-01-25 15:52:03 +0000

    remove test as this logic is in utils

--- tests/test_default_fund_round.py
+++
@@ -1,8 +0,0 @@
-from config.envs.default import DefaultConfig
-from fsd_utils.config.commonconfig import CommonConfig
-
-def test_default_fund_id():
-    assert CommonConfig.COF_FUND_ID == DefaultConfig.DEFAULT_FUND_ID
-
-def test_default_round_id():
-    assert CommonConfig.COF_ROUND_2_W3_ID == DefaultConfig.DEFAULT_ROUND_ID
