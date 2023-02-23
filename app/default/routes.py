b44cc5aa2f5571067d5039042b9ebdc7535c109b
Ram <80714392+ramsharma-prog@users.noreply.github.com>
2023-02-23 10:48:23 +0000

    enforce language for fund and round name translation

--- app/default/routes.py
+++ app/default/routes.py
@@ -4,6 +4,8 @@ from config import Config
 from flask import Blueprint
 from flask import current_app
 from flask import render_template
+from fsd_utils.locale_selector.get_lang import get_lang
+from flask_babel import force_locale
 from fsd_utils.simple_utils.date_utils import (
     current_datetime_after_given_iso_string,
 )
@@ -15,15 +17,15 @@ default_bp = Blueprint("routes", __name__, template_folder="templates")
 @default_bp.route("/")
 def index():
     current_app.logger.info("Service landing page loaded.")
-    default_language = {"language": "en"}
+    language = {"language": get_lang()}
     try:
         round_data = get_round_data(
-            fund_id=Config.DEFAULT_FUND_ID, round_id=Config.DEFAULT_ROUND_ID,language=default_language,
+            fund_id=Config.DEFAULT_FUND_ID, round_id=Config.DEFAULT_ROUND_ID,language=language,
             as_dict=True
         )
 
         fund_data = get_fund_data(
-            fund_id=Config.DEFAULT_FUND_ID, language=default_language,
+            fund_id=Config.DEFAULT_FUND_ID, language=language,
             as_dict=True)
         fund_name = fund_data.name
         submission_deadline = round_data.deadline
@@ -34,15 +36,15 @@ def index():
         round_title = ""
         submission_deadline = ""
         contact_us_email_address = ""
-
-    return render_template(
-        "index.html",
-        service_url=Config.ENTER_APPLICATION_URL,
-        fund_name=fund_name,
-        round_title=round_title,
-        submission_deadline=submission_deadline,
-        is_past_submission_deadline=current_datetime_after_given_iso_string(
-            submission_deadline
-        ),
-        contact_us_email_address=contact_us_email_address,
-    )
+    with force_locale(get_lang()):
+        return render_template(
+            "index.html",
+            service_url=Config.ENTER_APPLICATION_URL,
+            fund_name=fund_name,
+            round_title=round_title,
+            submission_deadline=submission_deadline,
+            is_past_submission_deadline=current_datetime_after_given_iso_string(
+                submission_deadline
+            ),
+            contact_us_email_address=contact_us_email_address,
+        )
