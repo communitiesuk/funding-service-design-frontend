cade91397714b53984e123112757706222869341
Sarah Sloan <sarah.sloan@levellingup.gov.uk>
2023-03-29 12:11:30 +0100

    FS-2432 testing new start page

--- app/default/routes.py
+++ app/default/routes.py
@@ -1,3 +1,4 @@
+from app.default.data import determine_round_status
 from app.default.data import get_fund_data
 from app.default.data import get_fund_data_by_short_name
 from app.default.data import get_round_data
@@ -10,7 +11,6 @@ from flask import render_template
 from fsd_utils.simple_utils.date_utils import (
     current_datetime_after_given_iso_string,
 )
-
 
 default_bp = Blueprint("routes", __name__, template_folder="templates")
 
@@ -67,21 +67,20 @@ def index_fund_round(fund_short_name, round_short_name):
     )
     if not round_data:
         abort(404)
+    round_status = determine_round_status(round_data)
+    if round_status.not_yet_open:
+        abort(404)
 
     return render_template(
         "fund_start_page.html",
         service_url=Config.MAGIC_LINK_URL.format(
             fund_short_name=fund_short_name, round_short_name=round_short_name
         ),
-        fund_name=fund_data["name"],
-        fund_title=fund_data["title"],
-        round_title=round_data["title"],
-        submission_deadline=round_data["deadline"],
-        is_past_submission_deadline=current_datetime_after_given_iso_string(
-            round_data["deadline"]
-        ),
-        contact_us_email_address=round_data["contact_details"][
-            "email_address"
-        ],
-        prospectus_link=round_data["prospectus"],
+        fund_name=fund_data.name,
+        fund_title=fund_data.title,
+        round_title=round_data.title,
+        submission_deadline=round_data.deadline,
+        is_past_submission_deadline=round_status.past_submission_deadline,
+        contact_us_email_address=round_data.contact_details.email_address,
+        prospectus_link=round_data.prospectus,
     )
