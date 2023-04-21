14d1117555f576d816422b8d854a45ba12ca25fa
gio-karunakaran <105926039+gio-karunakaran@users.noreply.github.com>
2023-04-21 14:14:41 +0100

    Update app/default/data.py
    Co-authored-by: tferns <117724519+tferns@users.noreply.github.com>

--- app/default/data.py
+++ app/default/data.py
@@ -202,7 +202,7 @@ def get_round_data(fund_id, round_id, language=None, as_dict=False):
 
 def get_round_data_by_short_names(
     fund_short_name, round_short_name, as_dict=False
-) -> Round:
+) -> Round | dict:
     params = {"language": get_lang(), "use_short_name": "true"}
 
     request_url = Config.GET_ROUND_DATA_BY_SHORT_NAME_ENDPOINT.format(
