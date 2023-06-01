2aa1176aed6edec1bf85176b860bb389864a7edb
srh-sloan <srh-sloan@users.noreply.github.com>
2023-06-01 09:44:10 +0100

    Fs 2433 tasklist guidance (#341)
    * Fixing tests after adding new field
    
    * Removing unused fund_store responsees
    
    * FS-2433 updating tasklist to show guidance from fund store
    
    * using privacy notice variable
    
    * fs-2854-ns-service-name
    
    * adding all questions url to application_guidance
    
    * Fixing tests after adding new field
    
    * Removing unused fund_store responsees
    
    * FS-2433 updating tasklist to show guidance from fund store
    
    * using privacy notice variable
    
    * adding all questions url to application_guidance
    
    * fs-2847-all-questions-link
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * review comments
    
    * Fixing tests after adding new field
    
    * Removing unused fund_store responsees
    
    * FS-2433 updating tasklist to show guidance from fund store
    
    * using privacy notice variable
    
    * adding all questions url to application_guidance
    
    * Fixing tests after adding new field
    
    * review comments
    
    * fixes after rebase
    
    ---------
    
    Co-authored-by: Gio Karunakaran <gio.karunakaran@version1.com>
    Co-authored-by: gio-karunakaran <105926039+gio-karunakaran@users.noreply.github.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- tests/test_all_questions.py
+++ tests/test_all_questions.py
@@ -2,11 +2,12 @@ from bs4 import BeautifulSoup
 from werkzeug import test
 
 
-def test_all_questions_page_from_short_name(flask_test_client):
+def test_all_questions_page_from_short_name(flask_test_client, mocker):
+
     response: test.TestResponse = flask_test_client.get(
         "/all_questions/COF/R2W3?lang=en"
     )
     assert response.status_code == 200
     soup = BeautifulSoup(response.data, "html.parser")
     assert soup.find("h1").text == "Full list of application questions"
-    assert "Community Ownership Fund Round 2 Window 3" in soup.get_text()
+    assert "Community Ownership Fund closed_round" in soup.get_text()
