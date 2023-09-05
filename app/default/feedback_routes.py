c0b2cb669d7915fc215c41b9444751b6122ac3e2
Abu Bakr Navsa <abu_navsa@outlook.com>
2023-09-05 14:18:26 +0100

    removed unused imports

--- app/default/feedback_routes.py
+++ app/default/feedback_routes.py
@@ -1,14 +1,6 @@
-from app.helpers import find_fund_and_round_in_request
-from app.helpers import find_round_in_request
-from app.helpers import get_round
-from flask import abort
 from flask import Blueprint
 from flask import current_app
-from flask import redirect
 from flask import render_template
-from flask import request
-from flask import url_for
-from fsd_utils.authentication.decorators import login_requested
 
 content_bp = Blueprint(
     "feedback_routes", __name__, template_folder="templates"
