9d25b4dd06f60be459e3dea458cc726723e18887
AbuNavsa <60141291+AbuNavsa@users.noreply.github.com>
2023-10-24 14:21:10 +0100

    Fs-3392 feedback welsh translations (#416)
    * Added all welsh translations
    
    * fixed translations
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Added end of application translations
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * reverted
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * revert temp environment changes for test
    
    * tasklist fix
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * missing brackets
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- app/forms/feedback_forms.py
+++ app/forms/feedback_forms.py
@@ -1,3 +1,4 @@
+from flask_babel import gettext
 from flask_wtf import FlaskForm
 from wtforms import HiddenField
 from wtforms import IntegerField
@@ -5,6 +6,7 @@ from wtforms import RadioField
 from wtforms import TextAreaField
 from wtforms.validators import DataRequired
 from wtforms.validators import InputRequired
+from wtforms.validators import NumberRange
 
 
 # Always has an application_id, as it's required by @verify_application_owner_local on POST requests.
@@ -14,17 +16,31 @@ class ApplicationFlaskForm(FlaskForm):
 
 class DefaultSectionFeedbackForm(ApplicationFlaskForm):
     experience = RadioField(
-        "How easy did you find it to complete this section?",
-        choices=[
-            ("very easy", "Very easy"),
-            ("easy", "Easy"),
-            ("neither easy or difficult", "Neither easy nor difficult"),
-            ("difficult", "Difficult"),
-            ("very difficult", "Very difficult"),
-        ],
-        validators=[InputRequired(message="Select a score")],
+        label="How easy did you find it to complete this section?",
+        validators=[InputRequired(message=gettext("Select a score"))],
     )
-    more_detail = TextAreaField("Explain why you chose this score (optional)")
+    more_detail = TextAreaField(
+        label="Explain why you chose this score (optional)"
+    )
+
+    def __init__(self, *args, **kwargs):
+        super(DefaultSectionFeedbackForm, self).__init__(*args, **kwargs)
+        self.experience.label.text = gettext(
+            "How easy did you find it to complete this section?"
+        )
+        self.experience.choices = [
+            ("very easy", gettext("Very easy")),
+            ("easy", gettext("Easy")),
+            (
+                "neither easy or difficult",
+                gettext("Neither easy nor difficult"),
+            ),
+            ("difficult", gettext("Difficult")),
+            ("very difficult", gettext("Very difficult")),
+        ]
+        self.more_detail.label.text = gettext(
+            "Explain why you chose this score (optional)"
+        )
 
     @property
     def as_dict(self):
@@ -51,54 +67,93 @@ class EndOfApplicationPageForm(ApplicationFlaskForm):
 
 class EndOfApplicationPage1Form(EndOfApplicationPageForm):
     overall_application_experience = RadioField(
-        "How was your overall application experience?",
-        choices=[
-            ("very good", "Very good"),
-            ("good", "Good"),
-            ("average", "Average"),
-            ("poor", "Poor"),
-            ("very poor", "Very poor"),
-        ],
+        label="How was your overall application experience?",
         validators=[InputRequired(message="Select a score")],
     )
-    more_detail = TextAreaField("Explain why you chose this score (optional)")
+    more_detail = TextAreaField(
+        label="Explain why you chose this score (optional)"
+    )
+
+    def __init__(self, *args, **kwargs):
+        super(EndOfApplicationPage1Form, self).__init__(*args, **kwargs)
+        self.overall_application_experience.label.text = gettext(
+            "How was your overall application experience?"
+        )
+        self.overall_application_experience.choices = [
+            ("very good", gettext("Very good")),
+            ("good", gettext("Good")),
+            ("average", gettext("Average")),
+            ("poor", gettext("Poor")),
+            ("very poor", gettext("Very poor")),
+        ]
+        self.more_detail.label.text = gettext(
+            "Explain why you chose this score (optional)"
+        )
 
 
 class EndOfApplicationPage2Form(EndOfApplicationPageForm):
     demonstrate_why_org_funding = RadioField(
-        "To what extent do you agree that this application form allowed you to"
-        " demonstrate why your organisation should receive funding?",
-        choices=[
-            ("strongly agree", "Strongly agree"),
-            ("agree", "Agree"),
-            ("neither agree nor disagree", "Neither agree nor disagree"),
-            ("disagree", "Disagree"),
-            ("strongly disagree", "Strongly disagree"),
-        ],
+        label=(
+            "To what extent do you agree that this application form allowed"
+            " you to demonstrate why your organization should receive funding?"
+        ),
         validators=[InputRequired(message="Select a score")],
     )
+
+    def __init__(self, *args, **kwargs):
+        super(EndOfApplicationPage2Form, self).__init__(*args, **kwargs)
+        self.demonstrate_why_org_funding.label.text = gettext(
+            "To what extent do you agree that this application form allowed"
+            " you to demonstrate why your organization should receive funding?"
+        )
+        self.demonstrate_why_org_funding.choices = [
+            ("strongly agree", gettext("Strongly agree")),
+            ("agree", gettext("Agree")),
+            (
+                "neither agree nor disagree",
+                gettext("Neither agree nor disagree"),
+            ),
+            ("disagree", gettext("Disagree")),
+            ("strongly disagree", gettext("Strongly disagree")),
+        ]
 
 
 class EndOfApplicationPage3Form(EndOfApplicationPageForm):
     understand_eligibility_criteria = RadioField(
-        "How easy was it to understand the eligibility criteria for this"
-        " fund?",
-        choices=[
-            ("very easy", "Very easy"),
-            ("easy", "Easy"),
-            ("neither easy or difficult", "Neither easy nor difficult"),
-            ("difficult", "Difficult"),
-            ("very difficult", "Very difficult"),
-        ],
+        label=(
+            "How easy was it to understand the eligibility criteria for this"
+            " fund?"
+        ),
         validators=[DataRequired(message="Select a score")],
     )
+
+    def __init__(self, *args, **kwargs):
+        super(EndOfApplicationPage3Form, self).__init__(*args, **kwargs)
+        self.understand_eligibility_criteria.label.text = gettext(
+            "How easy was it to understand the eligibility criteria for this"
+            " fund?"
+        )
+        self.understand_eligibility_criteria.choices = [
+            ("very easy", gettext("Very easy")),
+            ("easy", gettext("Easy")),
+            (
+                "neither easy or difficult",
+                gettext("Neither easy nor difficult"),
+            ),
+            ("difficult", gettext("Difficult")),
+            ("very difficult", gettext("Very difficult")),
+        ]
 
 
 class EndOfApplicationPage4Form(EndOfApplicationPageForm):
     hours_spent = IntegerField(
-        "Number of hours spent:",
-        validators=[DataRequired(message="Enter number of hours")],
+        label="Number of hours spent:",
+        validators=[NumberRange(min=0, message="Enter number of hours")],
     )
+
+    def __init__(self, *args, **kwargs):
+        super(EndOfApplicationPage4Form, self).__init__(*args, **kwargs)
+        self.hours_spent.label.text = gettext("Number of hours spent:")
 
 
 END_OF_APPLICATION_FEEDBACK_SURVEY_PAGE_NUMBER_MAP = {
