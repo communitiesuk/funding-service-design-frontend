c8a28d41ec281ea27961e176494a74d178971022
Sarah Sloan <sarah.sloan@levellingup.gov.uk>
2024-02-23 14:48:07 +0000

    fix tests

--- tests/test_dashboard.py
+++ tests/test_dashboard.py
@@ -75,7 +75,7 @@ def test_dashboard_route_search_call(
 ):
     request_mock = mocker.patch("app.default.account_routes.request")
     request_mock.args.get = (
-        lambda key: fund_short_name if key == "fund" else (round_short_name if key == "round" else None)
+        lambda key, default: fund_short_name if key == "fund" else (round_short_name if key == "round" else default)
     )
     get_apps_mock = mocker.patch(
         "app.default.account_routes.search_applications",
