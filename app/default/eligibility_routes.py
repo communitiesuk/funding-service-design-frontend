cddced1e287ed4a2ecab967557b14716b0892564
NarenderRajuB <bnraju81@gmail.com>
2024-06-03 17:41:36 +0100

    rename eligibility result route

--- app/default/eligibility_routes.py
+++ app/default/eligibility_routes.py
@@ -12,19 +12,17 @@ from flask import url_for
 eligibility_bp = Blueprint("eligibility_routes", __name__, template_folder="templates")
 
 
-@eligibility_bp.route("/eligibility_result/<fund_name>/<round_name>", methods=["GET"])
+@eligibility_bp.route("/eligibility-result/<fund_name>/<round_name>", methods=["GET"])
 def eligiblity_result(fund_name, round_name):
     current_app.logger.info(f"Eligibility launch result: {fund_name} {round_name}")
     return_url = request.host_url + url_for("account_routes.dashboard", fund=fund_name, round=round_name)
     fund, round = get_fund_and_round(fund_short_name=fund_name, round_short_name=round_name)
-    current_app.logger.info(f"Eligibility retuurl url: {return_url}")
+    current_app.logger.info(f"Eligibility return url: {return_url}")
     return render_template("eligibility_result.html", fund_id=round.fund_id, round_id=round.id, backLink=return_url)
 
 
-@eligibility_bp.route("/launch_eligibility/<fund_id>/<round_id>", methods=["POST"])
+@eligibility_bp.route("/launch-eligibility/<fund_id>/<round_id>", methods=["POST"])
 def launch_eligibility(fund_id, round_id):
-    # TODO do something with the fund and round id here to find out what the form name should be
-    # It should be stored as part of the eligibility_config json in the round
     fund_details, round_details = get_fund_and_round(fund_id=fund_id, round_id=round_id)
     fund_name = fund_details.short_name.lower()
     round_name = round_details.short_name.lower()
@@ -36,9 +34,6 @@ def launch_eligibility(fund_id, round_id):
 
     current_app.logger.info(f"Url the form runner should return to '{return_url}'.")
 
-    # TODO do we need to retrieve an in-progress eligibility form here?
-    # TODO work out what to use for application ID as they don't have one yet -
-    # TODO think this might be used as part of the session identifier in form runner so is leaving it blank ok?
 
     rehydrate_payload = format_rehydrate_payload(
         form_data={"questions": []},
